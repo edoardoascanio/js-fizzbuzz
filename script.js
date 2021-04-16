7d136749ec054071958a13f2164c088a50e93170
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-16 09:14:32 +0200

    correzioni condizioni

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ for (var i = 1; i <= 100; i++) {
     var multiplo3 = i % 3 === 0
     var multiplo5 = i % 5 === 0
 
-    if ((numero = multiplo3) && (numero != multiplo5)) {
+    if (multiplo3 && !multiplo5) {
         console.log("Fizz")
     }
 
-    else if ((numero = multiplo5) && (numero != multiplo3))  {
+    else if (multiplo5 && !multiplo3) {
         console.log("Buzz")
     }
 
-    else if((numero = multiplo3)&&(numero = multiplo5)) {
+    else if (multiplo3 && multiplo5) {
         console.log("Fizz-Buzz")
     }
 
